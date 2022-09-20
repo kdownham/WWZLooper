7a86d1a438909a8b76e0e7b7592f62037b5d1c65
kdownham <kdownham@wisc.edu>
2022-09-20 14:26:28 -0700

    Fixed compilation errors

--- NanoCORE/WWZSelections.cc
+++ NanoCORE/WWZSelections.cc
@@ -1,6 +1,7 @@
 //#include "MuonSelections.h"
 //#include "IsolationTools.h"
 #include "Config.h"
+#include "WWZSelections.h"
 
 using namespace tas;
 
@@ -42,7 +43,7 @@ std::vector<pair<float,int>> get_el_resonance(std::vector<pair<int,int>> cand_el
     float el_Z_cand_mass = 999999999999999.;
     for ( int j = 0; j < cand_el_pairs.size(); j++ ){
 	  float m_ee = (Electron_p4().at(cand_el_pairs[j].first)+Electron_p4().at(cand_el_pairs[j].second)).M();
-	  if ( std::abs(m_ee - m_Z) < std::abs(el_Z_cand_mass - m_Z ){
+	  if ( std::abs(m_ee - m_Z) < std::abs(el_Z_cand_mass - m_Z) ){
 	       el_mass_index.clear();
                el_Z_cand_mass = m_ee;
 	       el_mass_index.emplace_back(m_ee,j);
@@ -57,7 +58,7 @@ std::vector<pair<float,int>> get_mu_resonance(std::vector<pair<int,int>> cand_mu
     float mu_Z_cand_mass = 999999999999999.;
     for ( int j = 0; j < cand_mu_pairs.size(); j++ ){
           float m_mm = (Muon_p4().at(cand_mu_pairs[j].first)+Muon_p4().at(cand_mu_pairs[j].second)).M();
-          if ( std::abs(m_mm - m_Z) < std::abs(mu_Z_cand_mass - m_Z ){
+          if ( std::abs(m_mm - m_Z) < std::abs(mu_Z_cand_mass - m_Z) ){
                mu_mass_index.clear();
                mu_Z_cand_mass = m_mm;
                mu_mass_index.emplace_back(m_mm,j);
@@ -73,7 +74,7 @@ std::vector<pair<int,int>> get_W_cands(bool is_ee, std::vector<pair<int,int>> Z_
            for ( int i = 0; i < Z_cand_vector.size(); i++ ){
 		for ( int k = 0; k < el_W_cands.size(); k++ ){
 		    if ( (el_W_cands[k] == Z_cand_vector[i].first) || (el_W_cands[k] == Z_cand_vector[i].second )) continue;
-                    W_cand.emplace_back(el_W_cands[k],Electron_pdgId().at(el_W_cands[k])));	 	 
+                    W_cand.emplace_back(el_W_cands[k],Electron_pdgId().at(el_W_cands[k]));	 	 
 		    
 		}
 	   }
@@ -82,20 +83,20 @@ std::vector<pair<int,int>> get_W_cands(bool is_ee, std::vector<pair<int,int>> Z_
            for ( int i = 0; i < Z_cand_vector.size(); i++ ){
 		for ( int k = 0; k < el_W_cands.size(); k++ ){
                     if ( (el_W_cands[k] == Z_cand_vector[i].first) || (el_W_cands[k] == Z_cand_vector[i].second )) continue;
-                    W_cand.emplace_back(el_W_cands[k],Electron_pdgId().at(el_W_cands[k])));        
+                    W_cand.emplace_back(el_W_cands[k],Electron_pdgId().at(el_W_cands[k]));        
                 }
            }
            for ( int k = 0; k < mu_W_cands.size(); k++ ){
-                    W_cand.emplace_back(mu_W_cands[k],Muon_pdgId().at(mu_W_cands[k]))); 
+                    W_cand.emplace_back(mu_W_cands[k],Muon_pdgId().at(mu_W_cands[k])); 
                 }
       }    
    }
    else{ 
-     if ( mu_W_cands.size() > 0 && el_W_cands.size == 0 ){
+     if ( mu_W_cands.size() > 0 && el_W_cands.size() == 0 ){
 	  for ( int p = 0; p < Z_cand_vector.size(); p++ ){
 	        for ( int q = 0; q < mu_W_cands.size(); q++ ){
                     if ( (mu_W_cands[q] == Z_cand_vector[p].first) || (mu_W_cands[q] == Z_cand_vector[p].second) ) continue;
-		    W_cand.emplace_back(mu_W_cands[q],Muon_pdgId().at(mu_W_cands[q])));
+		    W_cand.emplace_back(mu_W_cands[q],Muon_pdgId().at(mu_W_cands[q]));
 		}
 	  }
      }
@@ -103,7 +104,7 @@ std::vector<pair<int,int>> get_W_cands(bool is_ee, std::vector<pair<int,int>> Z_
 	  for ( int p = 0; p < Z_cand_vector.size(); p++ ){
                 for ( int q = 0; q < mu_W_cands.size(); q++ ){
                     if ( (mu_W_cands[q] == Z_cand_vector[p].first) || (mu_W_cands[q] == Z_cand_vector[p].second) ) continue;
-                    W_cand.emplace_back(mu_W_cands[q],Muon_pdgId().at(mu_W_cands[q])));
+                    W_cand.emplace_back(mu_W_cands[q],Muon_pdgId().at(mu_W_cands[q]));
                 }
           }
           for ( int r = 0; r < el_W_cands.size(); r++ ){
--- NanoCORE/WWZSelections.h
+++ NanoCORE/WWZSelections.h
@@ -9,3 +9,5 @@ std::vector<pair<int,int>> getCandPairs_mu(std::vector<int> mu_input);
 std::vector<pair<float,int>> get_el_resonance(std::vector<pair<int,int>> cand_el_pairs);
 std::vector<pair<float,int>> get_mu_resonance(std::vector<pair<int,int>> cand_mu_pairs);
 std::vector<pair<int,int>> get_W_cands(bool is_ee, std::vector<pair<int,int>> Z_cand_vector, std::vector<int> el_W_cands, std::vector<int> mu_W_cands);
+
+#endif
